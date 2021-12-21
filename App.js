6ce8726f3ae955effc79f3a6a1d967332be612eb
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-21 13:38:17 +0600

    inline styles

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [text, setText] = useState('Open up App.js to start working on your app!')
+  const [text, setText] = useState(
+    "Open up App.js to start working on your app!"
+  );
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change Text" onPress={() => setText('The text has been changed...!!!')} />
+    <View style={{ padding: 25 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Enter Course Goal"
+          style={{
+            borderWidth: 1,
+            borderColor: "gray",
+            padding: 5,
+            width: '80%'
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+
+      <View />
     </View>
   );
 }
@@ -16,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
