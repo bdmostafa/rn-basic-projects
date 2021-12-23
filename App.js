228c7a50ceefdfb6ced42319a5cfd09db2ffc2d8
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-23 13:55:41 +0600

    separate card component with layout and css

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StyleSheet, View } from "react-native";
 import { Header } from "./components/Header";
+import { StartGamesScreen } from "./screens/StartGamesScreen";
 
 export default function App() {
-
-
   return (
     <View style={styles.screen}>
-      <Header title="Guess A Number" />
+      <Header title="Guess a Number" />
+      <StartGamesScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
+    flex: 1
   },
 });
--- components/Header.js
+++ components/Header.js
@@ -14,12 +14,13 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: "#f7287b",
+    backgroundColor: "coral",
     alignItems: "center",
     justifyContent: "center",
   },
   headerTitle: {
-    color: "black",
-    fontSize: 18,
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
