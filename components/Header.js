d1ade056aaa6170a128b211b71c5334d0d39de82
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-26 00:02:57 +0600

    Platform API usages

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Colors from '../constants/colors'
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/colors";
 import { TitleText } from "./TitleText";
 
 export const Header = ({ title }) => {
@@ -16,12 +16,14 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
     alignItems: "center",
     justifyContent: "center",
+    borderBottomColor: Platform.OS === "ios" ? "#ccc" : "transparent",
+    borderBottomWidth: Platform.OS === "ios" ? 1 : 0,
   },
   headerTitle: {
-    color: "white",
+    color: Platform.OS === "ios" ? Colors.primary : "white",
     fontSize: 24,
   },
 });
