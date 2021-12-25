530301f5561538705ffa63a2c8b03157e4f548fb
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-25 18:21:35 +0600

    dimensions api used in gameoverscreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { BodyText } from "../components/BodyText";
 import { PrimaryButton } from "../components/PrimaryButton";
 import { TitleText } from "../components/TitleText";
@@ -7,35 +15,41 @@ import Colors from "../constants/colors";
 
 export const GameOverScreen = ({ roundsNumber, onRestart, userNumber }) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          //   source={require("../assets/gameover.jpg")}
-          fadeDuration={1000}
-          source={{
-            uri:
-              "https://media.gettyimages.com/videos/game-over-neon-sign-style-flashing-number-movement-animation-video-id1331247012?s=640x640",
-          }}
-          style={styles.image}
-          resizeMode="cover"
-        />
-      </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{" "}
-          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.highlight}>
-            {roundsNumber}{" "}
-          </Text>{" "}
-          rounds to guess the number{" "}
-          <Text style={styles.highlight}> {userNumber} </Text>{" "}
-        </BodyText>
-      </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText style={styles.title}>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            //   source={require("../assets/gameover.jpg")}
+            fadeDuration={1000}
+            source={{
+              uri:
+                "https://media.gettyimages.com/videos/game-over-neon-sign-style-flashing-number-movement-animation-video-id1331247012?s=640x640",
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{" "}
+            <Text
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              style={styles.highlight}
+            >
+              {roundsNumber}{" "}
+            </Text>{" "}
+            rounds to guess the number{" "}
+            <Text style={styles.highlight}> {userNumber} </Text>{" "}
+          </BodyText>
+        </View>
 
-      <View style={{ marginTop: 25 }}>
-        <PrimaryButton onPress={onRestart}>NEW GAME</PrimaryButton>
+        <View>
+          <PrimaryButton onPress={onRestart}>NEW GAME</PrimaryButton>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -45,14 +59,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  title: {
+    marginTop: Dimensions.get("window").height < 400 ? 10 : 20,
+  },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: Colors.primary,
     overflow: "hidden",
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 35,
   },
   image: {
     width: "100%",
@@ -60,11 +77,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 10,
+    marginVertical: Dimensions.get("window").height / 60,
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
   },
   highlight: {
     color: Colors.primary,
